ba1dbce91adf105ddda750eeebae2560a770ae20
cryptofunny724 <cryptofunny724@gmail.com>
2026-06-15 13:26:29 +0200

    Add contract.py v2.0 - Task Marketplace

--- contract.py
+++ contract.py
@@ -1,5 +1,5 @@
-# Task Marketplace v2.0
-# Task System with Automatic Rewards
+# v2.0
+# { "Depends": "py-genlayer:1jb45aa8ynh2a9c9xn3b7qqh8sm5q93hwfp7jqmwsfhh8jpz09h6" }
 
 from genlayer import *
 
@@ -7,12 +7,12 @@ class TaskMarketplace(gl.Contract):
     tasks: TreeMap[Address, str]
     reputation_address: Address
 
-    def __init__(self, rep_addr: Address):
-        self.reputation_address = rep_addr
+    def __init__(self):
+        # Hardcoded reputation address
+        self.reputation_address = Address("0x77f86b0D8A0230BD612F41F9c638d682f6aBc476")
 
     @gl.public.write
     def create_task(self, description: str) -> None:
-        """Create a new task"""
         user = gl.message.sender_address
         self.tasks[user] = description
         
@@ -25,16 +25,16 @@ class TaskMarketplace(gl.Contract):
 
     @gl.public.write
     def complete_task(self) -> None:
-        """Complete a task and earn bigger reward"""
         user = gl.message.sender_address
         
+        # Bigger reward for completing task
         try:
             rep = gl.contract(self.reputation_address)
-            rep.record_action(u256(20))  # Higher reward for completion
+            rep.record_action(u256(20))
         except:
             pass
 
     @gl.public.view
     def my_task(self) -> str:
-        """Get my current task"""
-        return self.tasks.get(gl.message.sender_address, "")
+        user = gl.message.sender_address
+        return self.tasks.get(user, "")
